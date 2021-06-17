03f8a1d07ae6c35228ff0ab37e799b358d4bdf41
PetykaTetyka <61200774+PetykaTetyka@users.noreply.github.com>
2021-06-18 00:06:36 +0300

    Create main.tf

---
+++ modules/s3/eu-central-1/main.tf
@@ -0,0 +1,15 @@
+module "labels" {
+  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=0.24.1"
+  context     = var.context
+  name        = var.name
+  label_order = var.label_order
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket" "this" {
+  bucket        = "${data.aws_caller_identity.current.account_id}-${module.labels.id}"
+  acl           = "private"
+  force_destroy = true
+  tags          = module.labels.tags
+}
