acbc8c878ac44d068d4f9c514b0d6ce9abf03563
PetykaTetyka <61200774+PetykaTetyka@users.noreply.github.com>
2021-06-18 00:03:24 +0300

    Create get-all-authors.js

---
+++ modules/lambda/eu-central-1/lambda-functions/get-all-authors/get-all-authors.js
@@ -0,0 +1,22 @@
+const AWS = require("aws-sdk");
+const dynamodb = new AWS.DynamoDB({
+  region: process.env.AWS_REGION,
+  apiVersion: "2012-08-10"
+});
+
+exports.handler = (event, context, callback) => {
+  const params = {
+    TableName: process.env.TABLE_NAME
+  };
+  dynamodb.scan(params, (err, data) => {
+    if (err) {
+      console.log(err);
+      callback(err);
+    } else {
+      const authors = data.Items.map(item => {
+        return { id: item.id.S, firstName: item.firstName.S, lastName: item.lastName.S };
+      });
+      callback(null, authors);
+    }
+  });
+};
