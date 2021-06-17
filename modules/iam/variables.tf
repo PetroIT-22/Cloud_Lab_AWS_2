26ddca9b278de8ef48b4ab91bee77116d7ae8b3a
PetykaTetyka <61200774+PetykaTetyka@users.noreply.github.com>
2021-06-18 00:03:02 +0300

    Create variables.tf

---
+++ modules/iam/variables.tf
@@ -0,0 +1,9 @@
+variable "dynamo_db_authors_arn" {
+  description = "role_lambda_arn"
+  type        = string
+}
+
+variable "dynamo_db_courses_arn" {
+  description = "role_lambda_arn"
+  type        = string
+}
