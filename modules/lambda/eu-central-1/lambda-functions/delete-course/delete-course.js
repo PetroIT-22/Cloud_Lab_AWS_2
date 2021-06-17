a2029c38d8eae4f0a5c7e50ab6ddca6bb0818fde
PetykaTetyka <61200774+PetykaTetyka@users.noreply.github.com>
2021-06-18 00:03:13 +0300

    Create delete-course.js

---
+++ modules/lambda/eu-central-1/lambda-functions/delete-course/delete-course.js
@@ -0,0 +1,24 @@
+const AWS = require("aws-sdk");
+const dynamodb = new AWS.DynamoDB({
+  region: process.env.AWS_REGION,
+  apiVersion: "2012-08-10"
+});
+
+exports.handler = (event, context, callback) => {
+  const params = {
+    Key: {
+      id: {
+        S: event.id
+      }
+    },
+    TableName: process.env.TABLE_NAME
+  };
+  dynamodb.deleteItem(params, (err, data) => {
+    if (err) {
+      console.log(err);
+      callback(err);
+    } else {
+      callback(null, data);
+    }
+  });
+};
